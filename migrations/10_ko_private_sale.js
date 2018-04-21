f349a40f6b69dfd0a732d4635050a2b07cac9f08
Roderik van der Veer <roderik@settlemint.com>
2018-04-21 07:41:31 +0200

    chore: more migrations

--- migrations/10_ko_private_sale.js
+++ migrations/10_ko_private_sale.js
@@ -5,17 +5,20 @@ async function performMigration(deployer, network, accounts) {
   const lockup = moment('2018-04-23 16:00:00+01:00').unix()
   const DeployedTokenSale = await TokenSale.deployed()
   await DeployedTokenSale.handleExternalBuyers(
-    ['0x26C9899eA2F8940726BbCC79483F2ce07989314E'],
-    [web3.toWei(161725.067385445)],
-    [web3.toWei(0)],
-    [lockup]
+    [
+      '0xddcfdb1dde1b3d05f7d6712d385c197f7a6ef62f',
+      '0xcf88f6e099f53faba1ebeaa5e1a09855007e2a42',
+    ],
+    [web3.toWei(201600), web3.toWei(177633)],
+    [web3.toWei(0), web3.toWei(0)],
+    [lockup, lockup]
   )
 }
 
 module.exports = function(deployer, network, accounts) {
   deployer
     .then(function() {
-      //return performMigration(deployer, network, accounts)
+      return performMigration(deployer, network, accounts)
     })
     .catch(error => {
       console.log(error)
