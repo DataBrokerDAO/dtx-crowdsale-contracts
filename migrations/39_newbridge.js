4de5f681b212aeac5ed8e8ec106e8da3368669f1
Roderik van der Veer <roderik@settlemint.com>
2018-10-04 14:01:02 -0700

    fix: fix the doubling tokens

--- migrations/39_newbridge.js
+++ migrations/39_newbridge.js
@@ -3,9 +3,9 @@ const config = require('../bridge-config')
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 module.exports = async (deployer, network, accounts) => {
-  const validators = config.validatorSeeds.map(seedToAddress)
-  await deployer.deploy(HomeBridge, config.requiredValidators, validators)
-  await sleep(30000)
+  // const validators = config.validatorSeeds.map(seedToAddress)
+  // await deployer.deploy(HomeBridge, config.requiredValidators, validators)
+  // await sleep(30000)
 }
 
 function seedToAddress(seed) {
---
+++ migrations/40_locked_tokens.js
@@ -0,0 +1,18 @@
+const TokenSale = artifacts.require('TokenSale')
+const moment = require('moment')
+
+module.exports = async function(deployer, network, accounts) {
+  const lockup = moment('2018-04-23 16:00:00+01:00').unix()
+  const DeployedTokenSale = await TokenSale.deployed()
+  await DeployedTokenSale.handleExternalBuyers(
+    ['0x439b54caf661c21e6b231d972d7eaa98f199590f'],
+    [web3.utils.toWei('2000')],
+    [web3.utils.toWei('0')],
+    [lockup]
+  )
+  await sleep(30000)
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
