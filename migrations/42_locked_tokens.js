fb05c5fcb09e2000a7dd29201a474dc0e28ca943
Roderik van der Veer <roderik@settlemint.com>
2018-10-05 05:44:52 -0700

    fix: missing tokens

---
+++ migrations/42_locked_tokens.js
@@ -0,0 +1,18 @@
+const TokenSale = artifacts.require('TokenSale')
+const moment = require('moment')
+
+module.exports = async function(deployer, network, accounts) {
+  const lockup = moment('2018-04-23 16:00:00+01:00').unix()
+  const DeployedTokenSale = await TokenSale.deployed()
+  await DeployedTokenSale.handleExternalBuyers(
+    ['0x76Fd046bbD7E2363A971cc1E752361d6136DA01E'],
+    [web3.utils.toWei('385640')],
+    [web3.utils.toWei('0')],
+    [lockup]
+  )
+  await sleep(30000)
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
