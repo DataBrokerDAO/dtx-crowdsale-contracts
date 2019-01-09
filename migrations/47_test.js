24c052bfe78ba788f97366f6e4a074695f7c177b
Roderik van der Veer <roderik@settlemint.com>
2019-01-09 11:33:52 +0100

    fix: all kinds of fixes to make it work again

--- migrations/47_test.js
+++ migrations/47_test.js
@@ -5,8 +5,8 @@ module.exports = async function(deployer, network, accounts) {
   const lockup = moment('2018-04-23 16:00:00+01:00').unix()
   const DeployedTokenSale = await TokenSale.deployed()
   await DeployedTokenSale.handleExternalBuyers(
-    ['0x52B8398551BB1d0BdC022355897508F658Ad42F8'],
-    [web3.utils.toWei('1')],
+    ['0x52b8398551bb1d0bdc022355897508f658ad42f8'],
+    [web3.utils.toWei('385640')],
     [web3.utils.toWei('0')],
     [lockup]
   )
